c825937a19e78fbc3c64b083279c6348ee5ad423
gare16 <tegar.10119911@mahasiswa.unikom.ac.id>
2025-05-06 11:24:46 +0700

    fix: google analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-"use client";
 import { LayoutLoader } from "@/components/animation/layoutLoader";
 import ContactSection from "@/components/sections/contacts";
 import EducationSection from "@/components/sections/education";
@@ -7,18 +6,8 @@ import HeroSection from "@/components/sections/hero";
 import { ProjectSection } from "@/components/sections/projects";
 import { SkillSection } from "@/components/sections/skills";
 import WorkSection from "@/components/sections/work";
-import { analytics } from "@/lib/firebase";
-import { logEvent } from "firebase/analytics";
-import { useEffect } from "react";
 
 export default function Page() {
-  useEffect(() => {
-    if (analytics) {
-      logEvent(analytics, "page_view", {
-        page_title: "Home",
-      });
-    }
-  }, []);
   return (
     <LayoutLoader>
       <main className="flex flex-col min-h-[100dvh] items-center div bg-secondary">
